201bc63d335de6201ffcc84f5f2c3a603c1ae87b
imakewebthings <caleb@imakewebthings.com>
2015-06-07 14:56:26 -0700

    Send message and images to socket

--- public/main.js
+++ public/main.js
@@ -1,20 +1,25 @@
 var sending = false
+var socket = new WebSocket(window.webSocketEndpoint)
 
 navigator.getMedia = navigator.getUserMedia ||
   navigator.webkitGetUserMedia ||
   navigator.mozGetUserMedia ||
   navigator.msGetUserMedia
 
-navigator.getMedia({
-  audio: false,
-  video: {
-    optional: [
-      { minWidth: 640 },
-      { minHeight: 480 },
-      { minAspecRatio: 4 / 3 }
-    ]
-  }
-}, streamSuccess, streamError)
+socket.onopen = initPage
+
+function initPage () {
+  navigator.getMedia({
+    audio: false,
+    video: {
+      optional: [
+        { minWidth: 640 },
+        { minHeight: 480 },
+        { minAspecRatio: 4 / 3 }
+      ]
+    }
+  }, streamSuccess, streamError)
+}
 
 function streamSuccess (stream) {
   var video = document.querySelector('video')
@@ -66,22 +71,26 @@ function captureImages (images, options, callback) {
       options.canvas.width,
       options.canvas.height
     )
+    images.push(options.canvas.toDataURL('image/jpeg'))
+    options.count -= 1
+    if (options.count) {
+      window.setTimeout(function () {
+        captureImages(images, options, callback)
+      }, 100)
+    } else {
+      callback(images)
+    }
   } catch (e) {
     console.log('Error capturing images ' + e.message)
-  }
-  images.push(options.canvas.toDataURL('image/jpeg'))
-  options.count -= 1
-  if (options.count) {
-    window.setTimeout(function () {
-      captureImages(images, options, callback)
-    }, 100)
-  } else {
-    callback(images)
+    sending = false
   }
 }
 
 function sendImages (images) {
-  images.forEach(function (image) {
-    console.log(image)
-  })
+  sending = false
+
+  socket.send(JSON.stringify({
+    text: document.getElementById('text').value,
+    images: images
+  }))
 }
